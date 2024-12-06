a980a7b6397573e16e51b7c6cd84092a368bef91
Vladimir Sidorov <v.sidorov29091988@gmail.com>
2024-12-06 22:10:40 +0700

    Initial backend - Add docker-compose file

---
+++ backend/src/config/index.ts
@@ -0,0 +1,10 @@
+export default () => ({
+  port: process.env.PORT,
+  db_port: process.env.POSTGRES_PORT,
+  db_user: process.env.POSTGRES_USER,
+  db_password: process.env.POSTGRES_PASSWORD,
+  db_host: process.env.POSTGRES_HOST,
+  db_name: process.env.POSTGRES_DB,
+  secret_jwt: process.env.SECRET,
+  expire_jwt: process.env.EXPIRE_JWT,
+});
